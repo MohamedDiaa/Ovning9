92456b164cb53c580e3b6bf602a134133b7adfbd
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2024-04-08 23:25:29 +0200

    solve Bish-bosh

---
+++ bish-bosh.js
@@ -0,0 +1,22 @@
+
+
+var toggle = true 
+
+for(i=1; i<=100 ; i++) {
+    let divisableBythree = i%3;
+    let divisableByFour = i%4;
+
+    if(divisableBythree == 0 &&divisableByFour == 0) {
+       console.log("Bish-Bosh");
+    }
+    else if(divisableBythree == 0 || divisableByFour == 0) {
+
+        if(toggle) console.log("Bish");
+        else console.log("Bosh");
+    }
+    else {
+        console.log(i);
+    }
+    toggle = !toggle
+}
+
