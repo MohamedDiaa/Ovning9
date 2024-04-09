c7d3dcae9f899a0c5178d439926e1b222cd5aae0
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2024-04-09 12:53:06 +0200

    add li

--- ToDoList/todo.js
+++ ToDoList/todo.js
@@ -1,17 +1,27 @@
 const addBtn = document.getElementById("add");
 const inputField = document.getElementById("inputText");
-const containerDiv = document.getElementById("divContainer");
+const itemsList = document.getElementById("itemsList");
 
-var id = 1
+var id = 0
 
 function addAction() {
+
+    id++;
 
     let value = inputField.value;
 
     console.log(value);
-    containerDiv.append(`<p>${value}</p>`);
+    itemsList.insertAdjacentHTML(`beforeend`,`<li id="item${id}">${value}</li>`);
+ 
+    const item = document.getElementById(`item${id}`);
 
+    item.addEventListener('click', itemClicked);
 }
 
 
 addBtn.addEventListener('click', addAction);
+
+function itemClicked(item) {
+    console.log("here cliicked");
+    
+}
